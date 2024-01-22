7b86244af67d44ba7c8b4a4bcd7573c8ea37eb88
Антон <61877084+AntonPrNone@users.noreply.github.com>
2024-01-23 00:07:23 +0300

    Финалочка

--- PracWinForms1/Form1.Designer.cs
+++ PracWinForms1/Form1.Designer.cs
@@ -36,117 +36,129 @@
             this.ProductTextBox = new System.Windows.Forms.TextBox();
             this.MaxTextBox = new System.Windows.Forms.TextBox();
             this.MinTextBox = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ArrayTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // SumButton
             // 
             this.SumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SumButton.Location = new System.Drawing.Point(124, 333);
+            this.SumButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SumButton.Location = new System.Drawing.Point(62, 135);
+            this.SumButton.Margin = new System.Windows.Forms.Padding(2);
             this.SumButton.Name = "SumButton";
-            this.SumButton.Size = new System.Drawing.Size(376, 147);
+            this.SumButton.Size = new System.Drawing.Size(188, 76);
             this.SumButton.TabIndex = 0;
             this.SumButton.Text = "Сумма";
             this.SumButton.UseVisualStyleBackColor = true;
-            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            this.SumButton.Click += new System.EventHandler(this.Button_Click);
             // 
             // ProductButton
             // 
             this.ProductButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ProductButton.Location = new System.Drawing.Point(124, 538);
+            this.ProductButton.Location = new System.Drawing.Point(62, 242);
+            this.ProductButton.Margin = new System.Windows.Forms.Padding(2);
             this.ProductButton.Name = "ProductButton";
-            this.ProductButton.Size = new System.Drawing.Size(376, 147);
+            this.ProductButton.Size = new System.Drawing.Size(188, 76);
             this.ProductButton.TabIndex = 1;
             this.ProductButton.Text = "Произведение";
             this.ProductButton.UseVisualStyleBackColor = true;
-            this.ProductButton.Click += new System.EventHandler(this.ProductButton_Click);
+            this.ProductButton.Click += new System.EventHandler(this.Button_Click);
             // 
             // MinButton
             // 
             this.MinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MinButton.Location = new System.Drawing.Point(124, 938);
+            this.MinButton.Location = new System.Drawing.Point(62, 450);
+            this.MinButton.Margin = new System.Windows.Forms.Padding(2);
             this.MinButton.Name = "MinButton";
-            this.MinButton.Size = new System.Drawing.Size(376, 147);
+            this.MinButton.Size = new System.Drawing.Size(188, 76);
             this.MinButton.TabIndex = 2;
             this.MinButton.Text = "Минимум";
             this.MinButton.UseVisualStyleBackColor = true;
-            this.MinButton.Click += new System.EventHandler(this.MinButton_Click);
+            this.MinButton.Click += new System.EventHandler(this.Button_Click);
             // 
             // MaxButton
             // 
             this.MaxButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MaxButton.Location = new System.Drawing.Point(124, 742);
+            this.MaxButton.Location = new System.Drawing.Point(62, 348);
+            this.MaxButton.Margin = new System.Windows.Forms.Padding(2);
             this.MaxButton.Name = "MaxButton";
-            this.MaxButton.Size = new System.Drawing.Size(376, 147);
+            this.MaxButton.Size = new System.Drawing.Size(188, 76);
             this.MaxButton.TabIndex = 2;
             this.MaxButton.Text = "Максимум";
             this.MaxButton.UseVisualStyleBackColor = true;
-            this.MaxButton.Click += new System.EventHandler(this.MaxButton_Click);
+            this.MaxButton.Click += new System.EventHandler(this.Button_Click);
             // 
             // SumTextBox
             // 
             this.SumTextBox.AllowDrop = true;
             this.SumTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SumTextBox.Location = new System.Drawing.Point(786, 382);
+            this.SumTextBox.Location = new System.Drawing.Point(393, 161);
+            this.SumTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.SumTextBox.Name = "SumTextBox";
-            this.SumTextBox.Size = new System.Drawing.Size(357, 50);
+            this.SumTextBox.Size = new System.Drawing.Size(180, 29);
             this.SumTextBox.TabIndex = 3;
             // 
             // ProductTextBox
             // 
             this.ProductTextBox.AllowDrop = true;
             this.ProductTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.ProductTextBox.Location = new System.Drawing.Point(786, 587);
+            this.ProductTextBox.Location = new System.Drawing.Point(393, 267);
+            this.ProductTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.ProductTextBox.Name = "ProductTextBox";
-            this.ProductTextBox.Size = new System.Drawing.Size(357, 50);
+            this.ProductTextBox.Size = new System.Drawing.Size(180, 29);
             this.ProductTextBox.TabIndex = 4;
             // 
             // MaxTextBox
             // 
             this.MaxTextBox.AllowDrop = true;
             this.MaxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MaxTextBox.Location = new System.Drawing.Point(786, 791);
+            this.MaxTextBox.Location = new System.Drawing.Point(393, 373);
+            this.MaxTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.MaxTextBox.Name = "MaxTextBox";
-            this.MaxTextBox.Size = new System.Drawing.Size(357, 50);
+            this.MaxTextBox.Size = new System.Drawing.Size(180, 29);
             this.MaxTextBox.TabIndex = 5;
             // 
             // MinTextBox
             // 
             this.MinTextBox.AllowDrop = true;
             this.MinTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.MinTextBox.Location = new System.Drawing.Point(786, 987);
+            this.MinTextBox.Location = new System.Drawing.Point(393, 475);
+            this.MinTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.MinTextBox.Name = "MinTextBox";
-            this.MinTextBox.Size = new System.Drawing.Size(357, 50);
+            this.MinTextBox.Size = new System.Drawing.Size(180, 29);
             this.MinTextBox.TabIndex = 6;
             // 
-            // textBox1
+            // ArrayTextBox
             // 
-            this.textBox1.AllowDrop = true;
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(185, 232);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(932, 50);
-            this.textBox1.TabIndex = 7;
+            this.ArrayTextBox.AllowDrop = true;
+            this.ArrayTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ArrayTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ArrayTextBox.Location = new System.Drawing.Point(92, 83);
+            this.ArrayTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.ArrayTextBox.Name = "ArrayTextBox";
+            this.ArrayTextBox.Size = new System.Drawing.Size(468, 29);
+            this.ArrayTextBox.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(319, 162);
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(120, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(705, 44);
+            this.label1.Size = new System.Drawing.Size(409, 30);
             this.label1.TabIndex = 8;
             this.label1.Text = " Введите массив чисел через запятую:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1273, 1181);
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(636, 577);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ArrayTextBox);
             this.Controls.Add(this.MinTextBox);
             this.Controls.Add(this.MaxTextBox);
             this.Controls.Add(this.ProductTextBox);
@@ -155,9 +167,9 @@
             this.Controls.Add(this.MinButton);
             this.Controls.Add(this.ProductButton);
             this.Controls.Add(this.SumButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,7 +185,7 @@
         private System.Windows.Forms.TextBox ProductTextBox;
         private System.Windows.Forms.TextBox MaxTextBox;
         private System.Windows.Forms.TextBox MinTextBox;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ArrayTextBox;
         private System.Windows.Forms.Label label1;
     }
 }
--- PracWinForms1/Form1.cs
+++ PracWinForms1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -6,37 +7,98 @@ namespace PracWinForms1
 {
     public partial class Form1 : Form
     {
-        int[] arr = { 1, 2, 3, 4 };
+        double[] arr = { 1, 2, 3, 4 }; // Используем double вместо int
+
         public Form1()
         {
             InitializeComponent();
+            // Устанавливаем культуру, где дробная часть разделяется точкой
+            CultureInfo culture = new CultureInfo("en-US"); // Здесь "en-US" - это культура США с точкой в качестве разделителя десятичной части
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+            UpdateArrayTextBox();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        // Метод, который будет вызываться при нажатии на кнопки
+        private void Button_Click(object sender, EventArgs e)
         {
-             
+            // Вызываем метод для обновления массива из текста в ArrayTextBox
+            UpdateArrayFromTextBox();
+
+            if (arr.Length > 0)
+            {
+                // В зависимости от кнопки обновляем соответствующие текстбоксы
+                if (sender == SumButton)
+                {
+                    SumTextBox.Text = arr.Sum().ToString();
+                }
+
+                else if (sender == ProductButton)
+                {
+                    double prod = 1;
+                    for (int i = 0; i < arr.Length; i++) prod *= arr[i];
+                    ProductTextBox.Text = prod.ToString();
+                }
+
+                else if (sender == MaxButton)
+                {
+                    MaxTextBox.Text = arr.Max().ToString();
+                }
+
+                else if (sender == MinButton)
+                {
+                    MinTextBox.Text = arr.Min().ToString();
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Ошибка ввода. Пустой массив");
+            }
         }
 
-        private void SumButton_Click(object sender, EventArgs e)
+        // Метод для обновления массива из текста в ArrayTextBox
+        private void UpdateArrayFromTextBox()
         {
-            SumTextBox.Text = arr.Sum().ToString();
+            string input = ArrayTextBox.Text;
+
+            // Разделяем строку на массив строк, используя запятую в качестве разделителя
+            string[] inputArray = input.Split(',');
+
+            // Фильтруем массив, удаляя пустые строки и строки, состоящие только из пробелов
+            inputArray = inputArray.Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+            // Создаем новый массив для хранения чисел
+            double[] newArray = new double[inputArray.Length]; // Используем double вместо int
+
+            // Преобразуем каждую строку в дробное число и добавляем в новый массив
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                // Удаляем лишние пробелы вокруг каждой строки
+                string trimmedValue = inputArray[i].Trim();
+
+                if (double.TryParse(trimmedValue, out double num)) // Используем double.TryParse вместо int.TryParse
+                {
+                    newArray[i] = num;
+                }
+
+                else
+                {
+                    MessageBox.Show("Ошибка ввода. Пожалуйста, введите корректные числа, разделенные запятой.");
+                    return;
+                }
+            }
+
+            // Присваиваем новый массив переменной arr
+            arr = newArray;
+
+            // Опционально: обновляем текстбокс с текущим массивом
+            UpdateArrayTextBox();
         }
 
-        private void ProductButton_Click(object sender, EventArgs e)
+        private void UpdateArrayTextBox()
         {
-            int prod = 1;
-            for (int i = 0; i < arr.Length; i++) prod *= arr[i];
-            ProductTextBox.Text = prod.ToString();
-        }
-
-        private void MaxButton_Click(object sender, EventArgs e)
-        {
-            MaxTextBox.Text = arr.Max().ToString();
-        }
-
-        private void MinButton_Click(object sender, EventArgs e)
-        {
-            MinTextBox.Text = arr.Min().ToString();
+            ArrayTextBox.Text = string.Join(", ", arr);
         }
     }
 }
