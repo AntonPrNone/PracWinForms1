f49671e9e784d941f083f90683c0acff7d6e4714
201904 <201904@WINMAC-3>
2024-01-22 14:51:45 +0300

    Добавьте файлы проекта.

---
+++ PracWinForms1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PracWinForms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SumButton = new System.Windows.Forms.Button();
+            this.ProductButton = new System.Windows.Forms.Button();
+            this.MinButton = new System.Windows.Forms.Button();
+            this.MaxButton = new System.Windows.Forms.Button();
+            this.SumTextBox = new System.Windows.Forms.TextBox();
+            this.ProductTextBox = new System.Windows.Forms.TextBox();
+            this.MaxTextBox = new System.Windows.Forms.TextBox();
+            this.MinTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SumButton
+            // 
+            this.SumButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SumButton.Location = new System.Drawing.Point(124, 333);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(376, 147);
+            this.SumButton.TabIndex = 0;
+            this.SumButton.Text = "Сумма";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // ProductButton
+            // 
+            this.ProductButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProductButton.Location = new System.Drawing.Point(124, 538);
+            this.ProductButton.Name = "ProductButton";
+            this.ProductButton.Size = new System.Drawing.Size(376, 147);
+            this.ProductButton.TabIndex = 1;
+            this.ProductButton.Text = "Произведение";
+            this.ProductButton.UseVisualStyleBackColor = true;
+            this.ProductButton.Click += new System.EventHandler(this.ProductButton_Click);
+            // 
+            // MinButton
+            // 
+            this.MinButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinButton.Location = new System.Drawing.Point(124, 938);
+            this.MinButton.Name = "MinButton";
+            this.MinButton.Size = new System.Drawing.Size(376, 147);
+            this.MinButton.TabIndex = 2;
+            this.MinButton.Text = "Минимум";
+            this.MinButton.UseVisualStyleBackColor = true;
+            this.MinButton.Click += new System.EventHandler(this.MinButton_Click);
+            // 
+            // MaxButton
+            // 
+            this.MaxButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaxButton.Location = new System.Drawing.Point(124, 742);
+            this.MaxButton.Name = "MaxButton";
+            this.MaxButton.Size = new System.Drawing.Size(376, 147);
+            this.MaxButton.TabIndex = 2;
+            this.MaxButton.Text = "Максимум";
+            this.MaxButton.UseVisualStyleBackColor = true;
+            this.MaxButton.Click += new System.EventHandler(this.MaxButton_Click);
+            // 
+            // SumTextBox
+            // 
+            this.SumTextBox.AllowDrop = true;
+            this.SumTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SumTextBox.Location = new System.Drawing.Point(786, 382);
+            this.SumTextBox.Name = "SumTextBox";
+            this.SumTextBox.Size = new System.Drawing.Size(357, 50);
+            this.SumTextBox.TabIndex = 3;
+            // 
+            // ProductTextBox
+            // 
+            this.ProductTextBox.AllowDrop = true;
+            this.ProductTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProductTextBox.Location = new System.Drawing.Point(786, 587);
+            this.ProductTextBox.Name = "ProductTextBox";
+            this.ProductTextBox.Size = new System.Drawing.Size(357, 50);
+            this.ProductTextBox.TabIndex = 4;
+            // 
+            // MaxTextBox
+            // 
+            this.MaxTextBox.AllowDrop = true;
+            this.MaxTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaxTextBox.Location = new System.Drawing.Point(786, 791);
+            this.MaxTextBox.Name = "MaxTextBox";
+            this.MaxTextBox.Size = new System.Drawing.Size(357, 50);
+            this.MaxTextBox.TabIndex = 5;
+            // 
+            // MinTextBox
+            // 
+            this.MinTextBox.AllowDrop = true;
+            this.MinTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinTextBox.Location = new System.Drawing.Point(786, 987);
+            this.MinTextBox.Name = "MinTextBox";
+            this.MinTextBox.Size = new System.Drawing.Size(357, 50);
+            this.MinTextBox.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(185, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(932, 50);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(319, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(705, 44);
+            this.label1.TabIndex = 8;
+            this.label1.Text = " Введите массив чисел через запятую:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 1181);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.MinTextBox);
+            this.Controls.Add(this.MaxTextBox);
+            this.Controls.Add(this.ProductTextBox);
+            this.Controls.Add(this.SumTextBox);
+            this.Controls.Add(this.MaxButton);
+            this.Controls.Add(this.MinButton);
+            this.Controls.Add(this.ProductButton);
+            this.Controls.Add(this.SumButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button ProductButton;
+        private System.Windows.Forms.Button MinButton;
+        private System.Windows.Forms.Button MaxButton;
+        private System.Windows.Forms.TextBox SumTextBox;
+        private System.Windows.Forms.TextBox ProductTextBox;
+        private System.Windows.Forms.TextBox MaxTextBox;
+        private System.Windows.Forms.TextBox MinTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PracWinForms1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PracWinForms1
+{
+    public partial class Form1 : Form
+    {
+        int[] arr = { 1, 2, 3, 4 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            SumTextBox.Text = arr.Sum().ToString();
+        }
+
+        private void ProductButton_Click(object sender, EventArgs e)
+        {
+            int prod = 1;
+            for (int i = 0; i < arr.Length; i++) prod *= arr[i];
+            ProductTextBox.Text = prod.ToString();
+        }
+
+        private void MaxButton_Click(object sender, EventArgs e)
+        {
+            MaxTextBox.Text = arr.Max().ToString();
+        }
+
+        private void MinButton_Click(object sender, EventArgs e)
+        {
+            MinTextBox.Text = arr.Min().ToString();
+        }
+    }
+}
